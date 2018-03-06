d5d12793b16bc068f8f9528251d4f5f291aaa553
Sergey Koposov <skoposov@cmu.edu>
2018-03-06 15:50:42 -0500

    add first version of the simulation program

---
+++ mock_sim.py
@@ -0,0 +1,74 @@
+import astropy.table as atpy
+import read_girardi
+import numpy as np
+import scipy.spatial
+betw  = lambda x,x1,x2:(x>x1)&(x<x2)
+
+def getMagErr(mag, filt, survey='LSST'):
+    if survey == 'LSST':
+        import photErrorModel as pem
+        lem = pem.LSSTErrorModel()
+        #minmagerr = 0.01
+        magerr = lem.getMagError(mag,'LSST_'+filt)
+        return magerr
+
+def getMagErrVec(mag, filt, survey='LSST'):
+    maggrid = np.linspace(15,28,1000)
+    res = [getMagErr(m,filt,survey) for m in maggrid]
+    res=scipy.interpolate.UnivariateSpline(maggrid,res,s=0)(mag)
+    return res
+
+
+def getMagLimit(filt, survey='LSST'):
+    return 28
+
+def getIsoCurve(iso):
+    mini = iso['M_ini']
+    g=iso['DES-g']
+    r=iso['DES-r']
+    magstep = 0.01
+    res_g, res_r = [],[]
+    for i in range(len(mini)-1):
+        l_g,l_r = g[i],r[i]
+        r_g,r_r = g[i+1],r[i+1]
+        npt = max(abs(r_g-l_g),abs(r_r-l_r))/magstep+2
+        ggrid = np.linspace(l_g,r_g,npt)
+        rgrid = np.linspace(l_r,r_r,npt)
+        res_g.append(ggrid)
+        res_r.append(rgrid)
+    res_g, res_r = [np.concatenate(_) for _ in [res_g, res_r]]
+    return res_g, res_r
+
+    
+def get_mock_density(distance, isoname, survey,
+                         mockfile='stream_gap_mock.fits', mockarea=100):
+    """
+    Distance in kpc,
+    isochrone name
+    survey
+    """
+    minerr = 0.01
+    dat = atpy.Table().read(mockfile)
+    g, r = dat['g'], dat['r']
+    dm = 5*np.log10(distance*1e3)-5
+    iso = read_girardi.read_girardi(isoname)
+    xind = iso['stage']<=3
+    r_mag_limit = getMagLimit('r',survey)
+    gcurve, rcurve = getIsoCurve(iso)
+    gcurve,rcurve = [ _+ dm  for _ in [gcurve,rcurve]]
+    grgrid,rgrid = np.mgrid[-0.3:1.2:0.005,15:28:0.005]
+    ggrid = grgrid+rgrid
+    arr0 = np.array([(ggrid).flatten(),rgrid.flatten()]).T
+    arr = np.array([gcurve,rcurve]).T
+    tree = scipy.spatial.KDTree(arr)
+    D,xind=tree.query(arr0)
+    gerr = getMagErrVec(ggrid.flatten(), 'g', survey).reshape(ggrid.shape)
+    rerr = getMagErrVec(rgrid.flatten(), 'r', survey).reshape(rgrid.shape)
+    gerr,rerr = [np.maximum(_,minerr) for _ in [gerr,rerr]]
+    dg = ggrid-gcurve[xind].reshape(ggrid.shape)
+    dr = rgrid-rcurve[xind].reshape(rgrid.shape)
+    #1/0
+    thresh = 2 # 2 sigma
+    xind = (np.abs(dg/gerr)<2)&(np.abs(dr/rerr)<2)
+
+    return xind
