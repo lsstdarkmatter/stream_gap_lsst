c0622a87ddabdc22663700a7f1bfbffdfd5056d1
Nora Shipp <norashipp@uchicago.edu>
2018-10-30 11:01:22 -0700

    plot output file

--- vary_params.py
+++ vary_params.py
@@ -178,13 +178,56 @@ def make_plot(filename, mus=[30.], distances=[20.], velocities=[150.], impact_pa
                             plt.semilogy(mus, ret, 'o-', label=label)  # label='d = %d, w = %d, b = %d' % (distance, w, b)
 
     plt.legend()
-    plt.title('Minimum Detectable halo mass from a single stream impact')
+    plt.title(r'$\mathrm{Minimum\ Detectable\ Halo\ Mass}$')
     plt.xlabel(r'$\mu$ [mag/sq.arcsec]',)
     plt.ylabel(r'$M_{halo}$ [M$_{\odot}$]',)
     plt.savefig('%s.png' % filename)
 
 
-def plot_output():
+def plot_output(filename, mus=[30.], distances=[20.], velocities=[150.], impact_parameters=[1.], maglims=[None], latitudes=[60.], surveys=['LSST'], gap_fill=True):
+    output = np.genfromtxt('output.txt', unpack=True, delimiter=',', dtype=None, names=['dist', 'w', 'b', 'maglim', 'lat', 'gap_fill', 'survey', 'mu', 'mass'], encoding=None)
+
+    plt.figure()
+    for survey in surveys:
+        for lat in latitudes:
+            for maglim in maglims:
+                for b in impact_parameters:
+                    for w in velocities:
+                        for distance in distances:
+
+                            ret = []
+                            if maglim == None:
+                                maglim = mock_sim.getMagLimit('g', survey)
+
+                            for mu in mus:
+                                idx = (output['dist'] == dist) & (output['w'] == w) & (output['b'] == b) & (output['maglim']
+                                                                                                            == maglim) & (output['survey'] == survey) & (output['gap_fill'] == gap_fill) & (output['mu'] == mu)
+                                mass = output['mass'][idx][0]
+                                ret.append(mass)
+
+                            try:
+                                label = ''
+                                if len(distances) > 1:
+                                    label += ' d = %d kpc' % distance
+                                if len(velocities) > 1:
+                                    label += ' w = %d km/s' % w
+                                if len(impact_parameters) > 1:
+                                    label += ' b =  %d rs' % b
+                                if len(maglims) > 1:
+                                    label += ' maglim = %d' % maglim
+                                if len(latitudes) > 1:
+                                    label += ' lat = %d' % lat
+                                if len(surveys) > 1:
+                                    label += ' %s' % survey
+                            except:
+                                label = 'd=%d, w=%d, b=%d, mag=%d, lat=%d, %s' % (distance, w, b, maglim, lat, survey)
+                            plt.semilogy(mus, ret, 'o-', label=label)  # label='d = %d, w = %d, b = %d' % (distance, w, b)
+
+    plt.legend()
+    plt.title(r'$\mathrm{Minimum\ Detectable\ Halo\ Mass}$')
+    plt.xlabel(r'$\mu$ [mag/sq.arcsec]',)
+    plt.ylabel(r'$M_{halo}$ [M$_{\odot}$]',)
+    plt.savefig('%s.png' % filename)
 
 
 if __name__ == "__main__":
